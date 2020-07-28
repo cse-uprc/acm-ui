10e5efb5816eaabc66a5c049b21122cb4b120196
Samuel Butler <sambutler1017@icloud.com>
2020-07-28 11:17:50 -0400

    Version update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NumberAdderService } from 'acmkit/dist/acmkit-lib';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'acm-ui';
+  testValue = 0;
+
+  constructor(private numberAdderService: NumberAdderService) {}
+
+  ngOnInit() {
+    this.testValue = this.numberAdderService.addNumbers(3, 4);
+  }
 }
