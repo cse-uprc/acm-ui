bfc227e929ffa2106de6d52da8b38251dda6b17e
Jacob-Hunt <jacobhuntemail@gmail.com>
2020-07-30 12:42:17 -0400

    Created separate routes for home, login, and calendar pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'login', loadChildren: () => 
+      import('./login/login.module').then(m => m.LoginModule) 
+  },
+  { 
+    path: '', loadChildren: () => 
+      import('./home/home.module').then(m => m.HomeModule) 
+  },
+  { path: 'calendar', loadChildren: () => import('./calendar/calendar.module').then(m => m.CalendarModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
