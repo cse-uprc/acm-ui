5d15e319d6cdbf09acb341f51135e87b0e55a303
Sam Butler <67570772+sambutler1017@users.noreply.github.com>
2020-07-30 16:45:29 -0400

    Bug/acmui 22 add bootstrap styles (#23)
    * Adding bootstrap
    
    * Updates
    
    * Updating test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,13 @@
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AngularTestTitleComponent } from 'acmkit/dist/acmkit-lib';
+import { CardComponent } from 'acmkit/dist/acmkit-lib';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent, AngularTestTitleComponent],
+      declarations: [AppComponent, CardComponent],
     }).compileComponents();
   }));
 
