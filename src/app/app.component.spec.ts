7671e72ff00b667864a308f9712282b0e3154e66
Samuel Butler <sambutler1017@icloud.com>
2020-08-03 15:38:52 -0400

    Adding component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,22 @@
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { CardComponent } from 'acmkit/dist/acmkit-lib';
+import {
+  BasePageComponent,
+  CardComponent,
+  LoginCardComponent,
+} from 'acmkit/dist/acmkit-lib';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent, CardComponent],
+      declarations: [
+        AppComponent,
+        CardComponent,
+        LoginCardComponent,
+        BasePageComponent,
+      ],
     }).compileComponents();
   }));
 
