0046ec9f6ea9a0c0b87690abddca955164c9d9dc
Sam Butler <67570772+sambutler1017@users.noreply.github.com>
2020-08-03 15:55:06 -0400

    Updating UI (#32)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,22 @@
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { CardComponent } from 'acmkit/dist/acmkit-lib';
+import {
+  CardComponent,
+  LandingComponent,
+  LoginCardComponent,
+} from 'acmkit/dist/acmkit-lib';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent, CardComponent],
+      declarations: [
+        AppComponent,
+        CardComponent,
+        LoginCardComponent,
+        LandingComponent,
+      ],
     }).compileComponents();
   }));
 
