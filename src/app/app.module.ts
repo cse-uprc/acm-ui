569f4ed61b0039972f9330ce17b3fb22b5643961
DESKTOP-0MRSA9E\sambu <sambutler1017@icloud.com>
2021-01-05 20:37:42 -0500

    Bug Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AcmkitLibModule } from 'acmkit/dist/acmkit-lib';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './components/About-Us/about-us.component';
+import { ACMInformationComponent } from './components/ACM-Information/acm-information.component';
+import { BotServiceInfoComponent } from './components/Bot-Service-Info/bot-service-info.component';
+import { CalendarComponent } from './components/Calendar/calendar.component';
+import { ClassroomComponent } from './components/Classroom/classroom.component';
+import { ContactServicesComponent } from './components/Contact-Services/contact-services.component';
+import { DashboardComponent } from './components/Dashboard/dashboard.component';
+import { EventSignUpComponent } from './components/Event-Sign-Up/event-sign-up.component';
 import { HomeComponent } from './components/Home/home.component';
+import { ResourcesComponent } from './components/Resources/resources.component';
+import { StudentAdvisorsComponent } from './components/Student-Advisors/student-advisors.component';
+import { VideoLecturesComponent } from './components/Video-Lectures/video-lectures.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ACMInformationComponent,
+    BotServiceInfoComponent,
+    CalendarComponent,
+    ClassroomComponent,
+    ContactServicesComponent,
+    DashboardComponent,
+    EventSignUpComponent,
+    ResourcesComponent,
+    StudentAdvisorsComponent,
+    VideoLecturesComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, AcmkitLibModule],
   bootstrap: [AppComponent],
 })
